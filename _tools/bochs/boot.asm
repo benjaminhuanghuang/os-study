5f11df680a2e2466ea0f515e7c6f9eb38b3e51c2
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-03-04 20:28:52 -0800

    update

--- _tools/bochs/boot.asm
+++ _tools/bochs/boot.asm
@@ -13,7 +13,13 @@ int 0x10
 mov al, '!'
 int 0x10
 
+; set vga memory
+mov ax, 0xb800
+mov ds, ax        ; 不允许直接把立即数赋给段寄存器
 
+; set value to ds:0x00
+mov byte [0x00], 0x41         ; A
+mov byte [0x01], 0x04         ; black-red, no flicker
 
 jmp $
 
